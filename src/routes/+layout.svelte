d0444605d9226bf7f51ec718f5603e9b9e84e80d
Landon Tally <landonraytally+1@gmail.com>
2025-10-16 10:07:14 -0400

    logo + favicon updates

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -48,8 +48,12 @@
 <header class="bg-sky-700 shadow-md sticky top-0 z-50">
   <nav class="container mx-auto px-6 h-20 flex justify-between items-center">
     <div class="flex-shrink-0">
-      <a href="/" class="text-2xl font-bold text-white hover:text-indigo-200">
-        K&K Sales
+      <a href="/" class="block">
+        <img 
+          src="/logo.png" 
+          alt="K&K Sales Logo" 
+          class="h-12 w-auto transition-transform duration-300 ease-in-out hover:scale-110"
+        />
       </a>
     </div>
 
@@ -90,7 +94,7 @@
       <div class="flex items-center ml-8 pl-4">
         <a href="tel:812-334-1936" class="text-white hover:text-indigo-200 flex items-center gap-2">
           <svg class="w-5 h-5 flex-shrink-0" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z"></path></svg>
-          <span class="font-bold">812-334-1936</span>
+          <span class="font-bold text-lg">812-334-1936</span>
         </a>
       </div>
     </div>
